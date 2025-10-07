2bbc400c959bea2080043195cb755a8613d3d611
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-10-07 11:54:14 +0300

    Update email templates and email transport config
    Removed logo images and updated copyright in email templates to 'Skill Swap'. Changed default avatar in user model to .jpg. Updated email utility to use Gmail SMTP with credentials from environment variables instead of nodemailer test account.

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     avatar: {
       type: String,
-      default: "default-avatar.avif",
+      default: "default-avatar.jpg",
     },
     password: {
       type: String,
--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,23 @@
-import nodemailer from 'nodemailer';
-
-let testAccount;
-(async () => {
-  testAccount = await nodemailer.createTestAccount();
-})();
+import nodemailer from "nodemailer";
 
 class Email {
-  transporter = nodemailer.createTransport({
-    host: testAccount.smtp.host,
-    port: testAccount.smtp.port,
-    secure: testAccount.smtp.secure,
-    auth: {
-      user: testAccount.user,
-      pass: testAccount.pass
-    }
-  });
+  transporter;
 
   constructor(emailTemplatePath, name, userEmail, subject) {
     this.emailTemplatePath = emailTemplatePath;
     this.name = name;
     this.to = userEmail;
     this.subject = subject;
+
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GMAIL_USER, // your Gmail address
+        pass: process.env.APP_PASSWORD, // your Gmail App Password
+      },
+    });
   }
 }
 
